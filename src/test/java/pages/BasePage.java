7bd52e183dfa5d066c12b9b5da45c4742f44cec8
Anthony Arabia <tjay15787@gmail.com>
2023-04-15 17:46:05 +0200

    finished calendar functionality

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -7,9 +7,7 @@ import org.openqa.selenium.support.PageFactory;
 
 public class BasePage {
 
-    public void BasePage() {
-        PageFactory.initElements(Driver.getDriver(), this);
-    }
+
 
 
 }
